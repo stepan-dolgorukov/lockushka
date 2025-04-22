ba9e731ba7b2c64bd206eb6d377d158e4292cfd4
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2025-04-22 22:04:10 +0500

    Введение, применение переменной "FLAGS_COMPILER_C"

--- lockushka.mk
+++ lockushka.mk
@@ -1,7 +1,8 @@
 COMPILER_C := gcc
+FLAGS_COMPILER_C := -Og -g3
 
 lockushka: lockushka.o
 	"${COMPILER_C}" "${^}" -o "${@}"
 
 lockushka.o: lockushka.c
-	"${COMPILER_C}" -g3 -c "${<}"
+	"${COMPILER_C}" ${FLAGS_COMPILER_C} -c "${<}"
